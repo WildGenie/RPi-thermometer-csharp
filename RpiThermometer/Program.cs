21bc0ff8fb19fadb488c211943e5c2d4c92be6e9
Igor Kulman <igor@kulman.sk>
2014-11-24 10:31:24 +0100

    * init commit with OWIN

---
+++ RpiThermometer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace RpiThermometer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "http://localhost:5000";
+            using (WebApp.Start<Startup>(baseUrl))
+            {
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
