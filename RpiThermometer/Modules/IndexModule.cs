dc90704a2006a4fe7874c513e7b97ec4e1ce2e31
Igor Kulman <igor@kulman.sk>
2014-11-26 15:25:52 +0100

    * getting real temperature

--- RpiThermometer/Modules/IndexModule.cs
+++ RpiThermometer/Modules/IndexModule.cs
@@ -9,17 +9,41 @@ namespace RpiThermometer.Modules
 {
     public class IndexModule: NancyModule
     {
+        const string deviceId = "28-000004e23e98";
+
         public IndexModule()
         {
             Get["/"] = _ =>
             {
-                return View["index.html", 4];
+                return View["index.html", GetTemperature()];
             };
 
             Get["/temperature"] = _ =>
             {
-                return 5;
+                return GetTemperature();
             };
+        }
+
+        private string GetTemperature()
+        {
+            var si = new System.Diagnostics.Process
+            {
+                StartInfo =
+                {
+                    WorkingDirectory = "/",
+                    UseShellExecute = false,
+                    FileName = "cat",
+                    Arguments = " /sys/bus/w1/devices/" + deviceId + "/w1_slave",
+                    CreateNoWindow = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                }
+            };
+            si.Start();
+            string output = si.StandardOutput.ReadToEnd();
+            si.Close();
+            return output;
         }
     }
 }
